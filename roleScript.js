4f77797c877123f132a9d121d3633a65a928555f
Aayush Bharti <aayushbh2002@gmail.com>
2023-11-18 01:22:30 +0530

    ui changes

---
+++ roleScript.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "UI-UX Designer",
+      "Backend Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
